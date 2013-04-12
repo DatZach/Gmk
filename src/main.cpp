ad0653bfbf76cb6ec767e5f2c14b0cc2b4a99841
Zach Reedy <paraton@hotmail.com>
2013-04-12 08:54:49 -0400

    Structured directories

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ *	main.cpp
+ *	GMK API Library Test
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "GMK API Library Test" << std::endl;
+
+	std::getchar();
+
+	return 0;
+}
